110d826f18c0c5a106db7940f5a6a9781d13eca1
fer braga <augustofbg@gmail.com>
2022-11-09 15:20:55 -0300

    feat: req 01 - fetch ok

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,21 @@
+import requests
+import time
+
+
 # Requisito 1
 def fetch(url):
-    """Seu código deve vir aqui"""
+
+    try:
+        response = requests.get(
+            url, headers={"user-agent": "Fake user-agent"}, timeout=3
+        )
+        time.sleep(1)
+        if response.status_code == 200:
+            return response.text
+        else:
+            return None
+    except requests.Timeout:
+        return None
 
 
 # Requisito 2
