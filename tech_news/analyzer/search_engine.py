c1ea08b7996ef5787e518651860d12075e60aa71
fer braga <augustofbg@gmail.com>
2022-11-15 11:43:49 -0300

    feat: req 07

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,11 +1,10 @@
 from tech_news.database import db
+from datetime import datetime
 
 
 def search_by_title(title):
 
-    response = db.news.find(
-        {"title": {"$regex": title, "$options": "i"}}
-    )
+    response = db.news.find({"title": {"$regex": title, "$options": "i"}})
 
     title_found = [(item["title"], item["url"]) for item in response]
 
@@ -14,7 +13,18 @@ def search_by_title(title):
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+
+    try:
+        new_format_date = (
+            datetime.strptime(date, "%Y-%m-%d").date().strftime("%d/%m/%Y")
+        )
+        response = db.news.find({"timestamp": new_format_date})
+
+        titles_found = [(item["title"], item["url"]) for item in response]
+
+        return titles_found
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 # Requisito 8
